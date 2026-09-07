42775d3c44317c593a84cc694253201ca675395f
daniilkozub-byte <276952666+daniilkozub-byte@users.noreply.github.com>
2026-09-07 22:14:36 +0300

    fix: wrap countdown in thread, add final message, ignore .idea

--- Threads/src/ThreadStateDemo.java
+++ Threads/src/ThreadStateDemo.java
@@ -1,10 +1,18 @@
 public class ThreadStateDemo {
     public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(()-> {
+            for (int i = 5; i >= 1; i--){
+                System.out.println("Залишилось: " + i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            System.out.println("Час вийшов!");
+        });
 
-        for (int i = 5; i >= 1; i--){
-            System.out.println("Залишилось: " + i);
-            Thread.sleep(1000);
-        }
+        thread.start();
 
 
     }
